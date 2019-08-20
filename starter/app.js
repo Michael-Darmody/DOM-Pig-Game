178b41dabb2c870a4fefc21149570f1cfe717ce1
Michael <darmodymichael@gmail.com>
2019-08-20 20:58:11 +0100

    Implemented hold button functionality, DRYd up areas of code

--- starter/app.js
+++ starter/app.js
@@ -41,17 +41,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
-    activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
-    roundScore = 0;
 
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    hideDie()
-
+    nextPlayer()
   };
 
   function hideDie() {
@@ -64,3 +55,42 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
       clearTimeout(hide);
   };
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  scores[activePlayer] += roundScore;
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  if(scores[activePlayer] >= 100) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  var diceDOM = document.querySelector('.dice');
+  activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  hideDie()
+
+  function hideDie() {
+      hide = setTimeout(function() {
+          diceDOM.style.display = 'none';
+      }, 1000);
+  };
+
+  function showDie() {
+      clearTimeout(hide);
+  };
+};
